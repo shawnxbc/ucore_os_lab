fa6b3f6162794e549871af81b841bdb1edc48118
shawn <xiangbcyx@qq.com>
2020-09-10 23:21:09 +0800

    little fix

--- labcodes_answer/lab1_result/kern/init/init.c
+++ labcodes_answer/lab1_result/kern/init/init.c
@@ -85,7 +85,8 @@ static void
 lab1_switch_to_user(void) {
     //LAB1 CHALLENGE 1 : TODO
 	asm volatile (
-	    "sub $0x8, %%esp \n"
+	    "pushl %%ss \n"
+        "pushl %%esp \n"
 	    "int %0 \n"
 	    "movl %%ebp, %%esp"
 	    : 
