ece32ce36042855f2132259a6178d50726792f73
shawn <xiangbcyx@qq.com>
2020-09-10 12:17:12 +0800

    Lab1-Challenge 1

--- labcodes/lab1/kern/trap/trap.c
+++ labcodes/lab1/kern/trap/trap.c
@@ -48,11 +48,12 @@ idt_init(void) {
       */
       extern uintptr_t __vectors[];
       for (int i = 0; i < 256; i++) {
-          SETGATE(idt[i], 0, KERNEL_CS, __vectors[i], DPL_KERNEL);
+          SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
       }
       // 这里对系统调用做特殊处理：系统调用的编号0x80已经在trap.h中定义为T_SYSCALL，我们把系统调用的istrap设为1，
       // 把描述符权限级别设置为3，因为系统调用是向普通用户进程暴露的服务接口，权限应该设置为用户级别。
-      SETGATE(idt[T_SYSCALL], 1, KERNEL_CS, __vectors[T_SYSCALL], DPL_USER);
+      SETGATE(idt[T_SYSCALL], 1, GD_KTEXT, __vectors[T_SYSCALL], DPL_USER);
+      SETGATE(idt[T_SWITCH_TOK], 0, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
       lidt(&idt_pd);
 }
 
@@ -167,11 +168,24 @@ trap_dispatch(struct trapframe *tf) {
         c = cons_getc();
         cprintf("kbd [%03d] %c\n", c, c);
         break;
-    //LAB1 CHALLENGE 1 : YOUR CODE you should modify below codes.
+        
+    // LAB1 CHALLENGE 1 : YOUR CODE you should modify below codes.
     case T_SWITCH_TOU:
-    case T_SWITCH_TOK:
-        panic("T_SWITCH_** ??\n");
+        if (tf->tf_cs != USER_CS) {
+            tf->tf_cs = USER_CS;
+            tf->tf_ds = tf->tf_es = tf->tf_ss = USER_DS;
+            tf->tf_eflags |= FL_IOPL_MASK;
+        }
         break;
+
+    case T_SWITCH_TOK:
+        if (tf->tf_cs != KERNEL_CS) {
+            tf->tf_cs = KERNEL_CS;
+            tf->tf_ds = tf->tf_es = tf->tf_ss = KERNEL_DS;
+            tf->tf_eflags &= ~FL_IOPL_MASK;
+        }
+        break;
+
     case IRQ_OFFSET + IRQ_IDE1:
     case IRQ_OFFSET + IRQ_IDE2:
         /* do nothing */
